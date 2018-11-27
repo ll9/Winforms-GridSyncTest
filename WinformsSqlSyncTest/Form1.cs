4bb0d13e10018bb70737f7d4ea91f3b411d780c0
ll9 <llwierer@web.de>
2018-11-27 22:24:32 +0100

    Basic Buttons

--- WinformsSqlSyncTest/Form1.cs
+++ WinformsSqlSyncTest/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinformsSqlSyncTest.Utils;
 
 namespace WinformsSqlSyncTest
 {
     public partial class Form1 : Form
     {
+        private SqliteHandler _sqliteService = SqliteFactory.CreateInstance();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _sqliteService.CreateTable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _sqliteService.InsertRandomPerson();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _sqliteService.DropTable();
         }
     }
 }
